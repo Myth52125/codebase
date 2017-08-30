69e1b9327a35effbcf7427d16b9ab447998fdc74
myth52125 <myth52125@hotmail.com>
2017-08-30 12:21:38 +0800

    commplete timestamp

--- base/Timestamp.cc
+++ base/Timestamp.cc
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <iostream>
+using namespace myth52125;
 Timestamp::Timestamp()
+	:Timestamp(now())
+{}
+int64_t Timestamp:: now()
 {
 	struct timeval tv;
 	gettimeofday(&tv,NULL);
 	int64_t sec = tv.tv_sec;
-	std::cout<<" () "<<sec*mircoPerSecond+static_cast<int>(tv.tv_usec)<<std::endl;
-	Timestamp(sec*mircoPerSecond+static_cast<int>(tv.tv_usec));
+	return sec*mircoPerSecond+static_cast<int>(tv.tv_usec);
 
 }
 
@@ -16,25 +19,27 @@ Timestamp::Timestamp()
 Timestamp::Timestamp(time_t t,int micro)
 	:Timestamp(static_cast<int64_t>(t)+micro)
 {
-	std::cout<<"call me"<<std::endl;
 }
 
 std::string Timestamp::toString(bool showMirco)
 {
+	std::cout<<"tostring :"<<&_mircosecond<<std::endl;
+		
 	struct tm t;
-	time_t second = _mircosecond / mircoPerSecond;
+	time_t second =static_cast<time_t>( _mircosecond / mircoPerSecond);
 	gmtime_r(&second ,&t);
-
+	
+	std::cout<<"hour"<<t.tm_hour<<"\n";
 	char buf[32] = {0};
 	if(showMirco)
 	{
 		int mirco = static_cast<int>(_mircosecond % mircoPerSecond);
-		snprintf(buf,sizeof(buf),"%4d%02d%02d %02d:%02d:%02d.%06d",
-				t.tm_year+1970,t.tm_mon+1,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec,mirco);
+		snprintf(buf,sizeof(buf), "%4d%02d%02d %02d:%02d:%02d.%06d",
+				t.tm_year+1900,t.tm_mon+1,t.tm_mday,t.tm_hour+8,t.tm_min,t.tm_sec,mirco);
 	}else{
 	
 		snprintf(buf,sizeof(buf),"%4d%02d%02d %02d:%02d:%02d",
-				t.tm_year+1970,t.tm_mon+1,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec);
+				t.tm_year+1970,t.tm_mon+1,t.tm_mday,t.tm_hour+8,t.tm_min,t.tm_sec);
 	
 	}
 	return std::string(buf);
--- base/Timestamp.h
+++ base/Timestamp.h
@@ -5,19 +5,23 @@
 #include <time.h>
 #include <inttypes.h>
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+namespace myth52125
+{
+
 class Timestamp
 {
 private:
-	int64_t _mircosecond;
-
+	int64_t _mircosecond{0};
+	int64_t now();
 public:
 	Timestamp();
 	
 	Timestamp(int64_t t)
 		:_mircosecond(t)
 	{
-//		_mircosecond = t;
-		std::cout<<"in construstor t: "<<t<<" "<<_mircosecond<<std::endl;
+		std::cout<<"in construstor t: "<<&_mircosecond<<std::endl;
 	}
 
 	Timestamp(time_t ,int micro);
@@ -35,10 +39,21 @@ public:
 		
 	int64_t mircosecond()
 	{
-		
+//		showbytes((unsigned char *)&_mircosecond,sizeof(_mircosecond));
 		return _mircosecond;
 	}
-	
+
+
+	void showbytes(unsigned char *buf ,int len)
+	{
+		for(int i = 0; i<len;i++)
+		{
+			std::cout<<""<<(int)buf[i]<<" ";
+		}
+		std::cout<<std::endl;
+	}
+
+
 
 	std::string toString(bool showMirco = true);
 
@@ -62,5 +77,45 @@ inline bool operator==(Timestamp &l,Timestamp &r)
 	return l.mircosecond()	== r.mircosecond()	;
 	
 }
+inline double timeDiffence(const Timestamp &h, const Timestamp &l)
+{
+	int64_t diffence = h.mircosecond() -l.mircosecond();
+	return static_cast<double>(diffence/mircoPerSecond);
 
+
+
+}
+
+class TimeConsume
+{
+private:
+	Timestamp _time;
+public:
+	TimeConsume()
+		:_time(Timestamp())
+	{}
+	~TimeConsume()
+	{
+		status();
+	}
+
+	double status(bool print == true)
+	{
+		Timestamp now();
+		double diffence = now.mircosecond() - _time.mircosecond();
+		if(print)
+		{
+			std::cout<<"From the beginning to now after "<<diffence<<" seconds. "<<std::endl;
+		}
+		
+		return diffence;
+	}
+
+
+
+
+};
+
+}
 #endif
+
